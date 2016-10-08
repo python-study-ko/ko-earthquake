2fb0f1c0c2e46c7ee5f75760d28b2fb1da885f0f
sinsky <wesky93@gmail.com>
2016-10-08 10:04:19 +0900

    DB 명시파일 추가

--- data/checak_safezone/conect_sample.py
+++ data/checak_safezone/conect_sample.py
@@ -9,11 +9,14 @@ DB연결 작업시 아래 코드를 참고하세요
 # 지진대피소 원본 DB연결
 engine = create_engine('sqlite:///origin_safezone.sqlite', echo=True)
 
+"""
 # 지진대피소DB에서 대피소 ID, 이름, 수용 인원, 주소, 위,경도 를 추출 하는 쿼리
 with engine.connect() as db:
     result = db.execute("select shel_id,shel_nm,shel_av,shel_ad,lat,lon from safezone")
     for row in result:
         print(row)
+
+"""
 
 
 # 테이블 필드명을 국명으로 표시하여 처리하는 샘플코드 입니다. 추후 자료 가독성을 위해 구현 해봤습니다.
@@ -25,19 +28,22 @@ FildName = {
     "위도" : "lat",
     "경도" : "lon",
     "수용 인원" : "shel_av",
-    "주소" : "shel_av",
+    "주소" : "shel_ad",
     "생성일" : "create_dat",
     "행정구역 코드" : "b_area_cd", # 코드 출처 확인 불가(행정동, 법정동 코드 번호 모두 아님)
 }
 
 with engine.connect() as db:
     # 추출할 필드
-    choice_fild = ['번호', '이름', '수용 인원']
+    choice_fild = ['번호', '이름', '수용 인원','경도','위도','주소']
     filds = [ FildName[x] for x in choice_fild]
     result = db.execute("select {filds} from safezone".format(filds=",".join(filds)))
+    n = 0
     for row in result:
+        """
+        if n == 5:
+            break
+        n += 1
+        """
         print(row)
-
-
-
 
