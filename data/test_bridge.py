422ee683e08f33d10afaf9f52dbf27bee7ce103d
sinsky <wesky93@gmail.com>
2016-10-08 13:31:33 +0900

    윈도우에서 json 호출 오류(cp949) 해결
    파일을 불러올떄 utf-8으로 인코딩하도록 명시함

--- data/test_bridge.py
+++ data/test_bridge.py
@@ -1,7 +1,6 @@
 from data.bridge import Bridge
 
 # 테스트 파일
-# 할일:  윈도우에서 json 호출시 UnicodeDecodeError: 'cp949' codec can't decode byte 0xec in position 134: illegal multibyte sequence 에러 발생
 testDB = Bridge("ini_sample.json")
 
 
