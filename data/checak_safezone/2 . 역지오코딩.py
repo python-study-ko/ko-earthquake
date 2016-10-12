bb4c1034fe0f50447f9b68162b1fb49e28503642
sinsky <wesky93@gmail.com>
2016-10-12 12:19:50 +0900

    역지오코딩후 영문주소를 국문우로 변환해주는 코드 테스트
    주소를 국문으로 바꾸는 과정에서 간혹 오류가 있지만 크롤이기때문에 추후 수작업으로 변환하려고함

--- data/checak_safezone/2 . 역지오코딩.py
+++ data/checak_safezone/2 . 역지오코딩.py
@@ -14,8 +14,10 @@ API URL : http://apis.vworld.kr/geocode?service=reverse&apiKey=인증키&[검색
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from data.checak_safezone.safezone_db import *
+from data.checak_safezone.google_ad_trans import trans_ad
 import requests
 import configparser
+from pprint import pprint
 
 ini = configparser.ConfigParser()
 ini.read("API_KEY.ini")
@@ -36,7 +38,7 @@ engine = create_engine('sqlite:///check_safezone.sqlite')
 Session = sessionmaker(bind=engine)
 session = Session()
 
-data = session.query(POI).all()[:20]
+data = session.query(POI).all()[:10]
 all = len(data)
 
 # 구글 테스트 코드
@@ -62,18 +64,17 @@ def google_ad(lat, lon):
         return "실패: 에러발생 {}".format(e)
 
 
+# 역 지오코딩 테스트
+# 역지오코딩 결과 확인용
+g_ad_li = []    # 구글 역지오코딩 결과
+g_count = 0     # 총 작업 횟수
+g_ok = 0        # 성공 횟수
+g_fail = 0      # 실패 횟수
 
-# 테스트 결과 확인용 변수
-g_ad_li = []
-g_count = 0
-g_ok = 0
-g_fail = 0
-
-print("google 결과")
+print("google 역지오코딩 결과")
 for poi in data:
     g_count += 1
-    print(g_count,"/",all)
-    print(poi,"변환 요청")
+    print(g_count,"/",all," : ",poi,"변환 요청")
     ad = google_ad(poi.lat,poi.lon)
     if ad[:2] == "실패":
         g_fail += 1
@@ -82,13 +83,38 @@ for poi in data:
     print("변환 주소: {}".format(ad))
     g_ad_li.append(ad)
 
+# 영문->국문 주소변환 작업
+trans_ad_li = []    # 구글 국문 주소변환 결과
+trans_count = 0     # 총 작업 횟수
+trans_ok = 0        # 성공 횟수
+trans_fail = 0      # 실패 횟수
 
+print("google 주소 변환 결과")
+for ad_en in g_ad_li:
+    trans_count += 1
+    print(trans_count,"/",all," : ",ad_en,"주소 변환 크롤")
+    result,ad_ko = trans_ad(ad_en)
 
+    if result in [0,2]:
+        trans_fail += 1
+        print(ad_ko)
+        trans_ad_li.append(ad_ko)
+    else:
+        trans_ok += 1
+        print("변환 주소: {}".format(ad_ko))
+    trans_ad_li.append(ad_ko)
 
+# 영문,국문 주소 결과 합치기
+ad_merge = {}
+for ad_en,ad_ko in zip(g_ad_li,trans_ad_li):
+    ad_merge[ad_en] = ad_ko
 
-
+print("====================================================================")
 print("구글 역지오코딩 결과/ 요청 :{0}  성공 : {1}, 실패 : {2}".format(all,g_ok,g_fail))
-
+print("구글 영문주소 변환 결과/ 요청 :{0}  성공 : {1}, 실패 : {2}".format(all,trans_ok,trans_fail))
+print("====================================================================")
+print("최종 변환 결과")
+print(ad_merge)
 """
 for poi in data:
     poi.address = address(poi.lat, poi.lon)
