6132c0b4cf17891ac918acbce3aa8a23e019597b
sinsky <wesky93@gmail.com>
2016-10-12 16:09:07 +0900

    구글, 브이월드 코드 제거
    다음에서 정확한 역지오코딩이 가능하여 이전 두개 api 연동 코드를 제거함

--- data/checak_safezone/2 . 역지오코딩.py
+++ data/checak_safezone/2 . 역지오코딩.py
@@ -22,99 +22,63 @@ from pprint import pprint
 ini = configparser.ConfigParser()
 ini.read("API_KEY.ini")
 
-
 # API 정리
-vworld_key = ini.get("vworld", "KEY")
-vworld_url = "http://apis.vworld.kr/geocode"
-
-google_key = ini.get("google","KEY")
-google_url = "https://maps.googleapis.com/maps/api/geocode/json"
-
-
-
+daum_key = ini.get("daum", "KEY")
+daum_url = "https://apis.daum.net/local/geo/coord2detailaddr"
 
 # 세션 연결
 engine = create_engine('sqlite:///check_safezone.sqlite')
 Session = sessionmaker(bind=engine)
 session = Session()
 
-data = session.query(POI).all()[:10]
+data = session.query(POI).all()[:100]
 all = len(data)
 
-# 구글 테스트 코드
-def google_ad(lat, lon):
+
+# 다음 테스트 코드
+def daum_ad(lat, lon):
     """
     경위도 좌표를 역지오코딩하여 주소를 취득
     :param lat: 위도(y)
     :param lon: 경도(x)
     :return: 주소 혹은 실패 문구
     """
-    parms = {"key": google_key,"latlng":"{},{}".format(lat,lon)}
+    parms = {"apikey": daum_key, "x": lon, "y": lat, "inputCoordSystem": "WGS84", "output": "json"}
     try:
-        r = requests.get(google_url, params=parms)
-        status = r.json()["status"]
-        results = r.json()["results"]
-
-        if status == "OK":
-            #return results[0]["address_components"] # 주소 쳬계별로 분류된 결과
-            return results[0]["formatted_address"] # 전체 주소
-        else:
-            return "실패: {}".format(status)
+        r = requests.get(daum_url, params=parms)
+        ad = r.json()["old"]["name"]
+        if ad == "":
+            return "실패: 주소가 조회되지 않습니다."
+        return ad
     except Exception as e:
         return "실패: 에러발생 {}".format(e)
 
 
-# 역 지오코딩 테스트
-# 역지오코딩 결과 확인용
-g_ad_li = []    # 구글 역지오코딩 결과
-g_count = 0     # 총 작업 횟수
-g_ok = 0        # 성공 횟수
-g_fail = 0      # 실패 횟수
+# 다음 지오코딩 테스트
+d_ad_li = []  # 다음 지오코딩 결과
+d_count = 0  # 총 작업 횟수
+d_ok = 0  # 성공 횟수
+d_fail = 0  # 실패 횟수
 
-print("google 역지오코딩 결과")
+print("다음 지오코딩 결과")
 for poi in data:
-    g_count += 1
-    print(g_count,"/",all," : ",poi,"변환 요청")
-    ad = google_ad(poi.lat,poi.lon)
+    d_count += 1
+    print(d_count, "/", all, " : ", poi, "변환 요청")
+    ad = daum_ad(poi.lat, poi.lon)
     if ad[:2] == "실패":
-        g_fail += 1
+        d_fail += 1
     else:
-        g_ok += 1
+        # 조회 성공시 db에 주소 입력
+        d_ok += 1
+
     print("변환 주소: {}".format(ad))
-    g_ad_li.append(ad)
+    d_ad_li.append(ad)
 
-# 영문->국문 주소변환 작업
-trans_ad_li = []    # 구글 국문 주소변환 결과
-trans_count = 0     # 총 작업 횟수
-trans_ok = 0        # 성공 횟수
-trans_fail = 0      # 실패 횟수
 
-print("google 주소 변환 결과")
-for ad_en in g_ad_li:
-    trans_count += 1
-    print(trans_count,"/",all," : ",ad_en,"주소 변환 크롤")
-    result,ad_ko = trans_ad(ad_en)
+print("==========================================================================")
+print("다음 지오코딩 결과/ 요청 :{0}  성공 : {1}, 실패 : {2}".format(all, d_ok, d_fail))
+print("==========================================================================")
 
-    if result in [0,2]:
-        trans_fail += 1
-        print(ad_ko)
-        trans_ad_li.append(ad_ko)
-    else:
-        trans_ok += 1
-        print("변환 주소: {}".format(ad_ko))
-    trans_ad_li.append(ad_ko)
-
-# 영문,국문 주소 결과 합치기
-ad_merge = {}
-for ad_en,ad_ko in zip(g_ad_li,trans_ad_li):
-    ad_merge[ad_en] = ad_ko
-
-print("====================================================================")
-print("구글 역지오코딩 결과/ 요청 :{0}  성공 : {1}, 실패 : {2}".format(all,g_ok,g_fail))
-print("구글 영문주소 변환 결과/ 요청 :{0}  성공 : {1}, 실패 : {2}".format(all,trans_ok,trans_fail))
-print("====================================================================")
-print("최종 변환 결과")
-print(ad_merge)
 """
 for poi in data:
     poi.address = address(poi.lat, poi.lon)
