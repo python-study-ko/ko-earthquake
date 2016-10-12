b0b31c0427a614adf96990abc01261c9a7842bad
sinsky <wesky93@gmail.com>
2016-10-12 11:38:51 +0900

    구글 영문주소 변환 코드 모듈화

--- data/checak_safezone/google_ad_trans.py
+++ data/checak_safezone/google_ad_trans.py
@@ -5,12 +5,18 @@ import requests
 from bs4 import BeautifulSoup
 
 url ='https://www.google.co.kr/search?q='
-data = '45 Sannyang-gil, Ganggyeong-eup, Nonsan, Chungcheongnam-do, South Korea'
 
-sourcecode = requests.get(url + data)
-plaintext = sourcecode.text
-soup = BeautifulSoup(plaintext, "html.parser")
-
-test = soup.find_all('b')[1].contents
-
-print(test)
+def trans_ad(ad_en):
+    """
+    영문주소를 입력하면 구글에 검색한뒤 bs4로 주소만 크롤해줍니다.
+    :param ad_en: 영문 주소
+    :return: 튜플 ( 성공여부, 결과 메시지) ; 성공시: 0, 실패시: 1
+    """
+    sourcecode = requests.get(url + ad_en)
+    try:
+        plaintext = sourcecode.text
+        soup = BeautifulSoup(plaintext, "html.parser")
+        ad_ko = soup.find_all('b')[1].contents[0]
+    except Exception as e:
+        return (0,"영문주소 변환에 실패 했습니다.")
+    return (1,ad_ko)
