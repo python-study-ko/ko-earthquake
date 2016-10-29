9e43164757b48593d802df24faed9953932e1a08
sinsky <wesky93@gmail.com>
2016-10-29 19:55:23 +0900

    법정동 전체 코드 자료 정리
    법정동코드에서 시군구,법정동(읍면동)를 추출하는 코드 추가
    주소의 길이로 구분하였으나 세종특별자시치, 광역시, 도의 주소 체계가 달라 길이가 아닌 주소의 마지막 문자로 분기하도록 변경해야함

---
+++ data/check_building_info/clear_BJD_text.py
@@ -0,0 +1,56 @@
+# 행정표준코드에서 다운로드 받은 우리나라 전체 법정동코드.txt 파일에서 필요한 코드만 추출하는 코드입니다.
+from pprint import pprint
+
+# 테스트할때 최대 반복회수 지정
+count_limit = 100
+
+# 시군구 코드
+sgg_CD = {}
+
+# 법정동 코드
+bjd_CD = {}
+
+
+def check(list):
+    """
+    코드 폐지여부를 확인후 코드와 주소를 분리한다.
+    :param list: 법정동코드.txt에서 불러들인 코드,주소 정보
+    :return: 존재하는 코드일경우 (코드,주소)
+    """
+    if list[-1] == '존재':
+        return (list[0], list[1:-1])
+    else:
+        return False
+
+
+# 법정동 코드 자료에서 현재 폐지되지 않는(존재하는) 법정동,시군구 코드 추출
+with open("법정동코드.txt", encoding='euc-kr') as data:
+    count = 0
+    for line in data:
+        #if count == count_limit:
+        #       break
+        count += 1
+        info = line.split()
+
+        # 시군구 코드
+        if len(info) in [3, 4]:
+            result = check(info)
+            if result:
+                CD, AD = result
+                # 세종특별자치시 필터
+                sgg_CD[CD[:5]] = AD
+
+        # 법정동 코드
+        elif len(info) in [5,6,7,8]:
+            result = check(info)
+            if result:
+                CD,AD = result
+                # 광역시외 시군구 필터
+                if AD[-1][-1] in ['시','군','구']:
+                    sgg_CD[CD[:5]] = AD
+                else:
+                    bjd_CD[CD] = AD
+
+    print(count)
+    print("시군구 코드 개수 : {0}, 법정동 코드 갯수: {1}".format(len(sgg_CD),len(bjd_CD)))
+    pprint(sgg_CD)
