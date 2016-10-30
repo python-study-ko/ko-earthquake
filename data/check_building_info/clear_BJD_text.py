537deb59babb7cef822f9276da9842d2d1a685cd
wesky93 <wesky93@gmail.com>
2016-10-31 02:42:56 +0900

    시군구,법정동 코드를 분리하여  json으로 저장

--- data/check_building_info/clear_BJD_text.py
+++ data/check_building_info/clear_BJD_text.py
@@ -1,5 +1,6 @@
 # 행정표준코드에서 다운로드 받은 우리나라 전체 법정동코드.txt 파일에서 필요한 코드만 추출하는 코드입니다.
 from pprint import pprint
+import json
 
 # 테스트할때 최대 반복회수 지정(0일시 전체 자료를 순회)
 count_limit = 0
@@ -44,7 +45,7 @@ with open("법정동코드.txt", encoding='euc-kr') as data:
             AD_size = AD[-1][-1]
 
             if AD_size in ['시','군','구','도']:
-                sgg_CD[CD] = AD
+                sgg_CD[CD[:5]] = AD
             else:
                 bjd_CD[CD] = AD
 
@@ -59,9 +60,6 @@ with open("법정동코드.txt", encoding='euc-kr') as data:
     # 결과 : ['시', '구', '동', '로', '가', '군', '읍', '리', '면', '도', ')']
     """
 
-
-
-
 print(count)
 sgg_count = len(sgg_CD)
 bjd_count = len(bjd_CD)
@@ -70,3 +68,10 @@ all_count = sgg_count+bjd_count
 print("존재 코드: {0}, 폐지 코드: {1}".format(all_count,count-all_count))
 print("시군구 코드 개수 : {0}, 법정동 코드 갯수: {1}".format(sgg_count,bjd_count))
 pprint(sgg_CD)
+
+# json으로 저장
+with open('sgg.json',mode='w',encoding='utf-8') as f:
+    json.dump(sgg_CD,f)
+
+with open('bjd.json',mode='w',encoding='utf-8') as f:
+    json.dump(bjd_CD, f)
